16bf22a060f7dbdb4a3f9cc1f78b904b0aaa007b
EricKwok <erickwok404@gmail.com>
2018-05-23 13:34:32 +0800

    update

--- update-v2ray.sh
+++ update-v2ray.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-date >> /var/log/v2ray/update.log
-bash <(curl -L -s https://install.direct/go.sh) >> /var/log/v2ray/update.log
+TIME=`date "+%y-%m-%d %H:%M"`
+DATE=`date "+%y-%m-%d"`
+COLOR_Yellow="\e[033m"
+LOG_PATH="/var/log/v2ray"
+UPDATE_LOG_PATH="${LOG_PATH}/update"
+MAXLOGDAY=20
+SCRIPT_URL="https://install.direct/go.sh"
+
+mkdir ${LOG_PATH}
+mkdir ${UPDATE_LOG_PATH}
+
+echo -e "${COLOR_Yellow}${TIME}" >> ${LOG_PATH}/${DATE}.log
+bash <(curl -L -s ${SCRIPT_URL}) >> ${LOG_PATH}/${DATE}.log
+find ${LOG_PATH} -mtime +${MAXLOGDAY} -name "*.log" -exec rm {} \; # Delete outdate logs
+
