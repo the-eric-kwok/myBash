13da06b2dd70699383cb8604d1ab5f8d9a98aba2
明明 <erickwok404@gmail.com>
2018-06-01 15:18:00 +0800

    little update

--- update-v2ray.sh
+++ update-v2ray.sh
@@ -2,6 +2,7 @@
 TIME=`date "+%y-%m-%d %H:%M"`
 DATE=`date "+%y-%m-%d"`
 COLOR_Yellow="\e[033m"
+COLOR_RESET="\e[0m"
 LOG_PATH="/var/log/v2ray"
 MAXLOGDAY=20
 SCRIPT_URL="https://install.direct/go.sh"
@@ -9,7 +10,7 @@ SCRIPT_URL="https://install.direct/go.sh"
 mkdir ${LOG_PATH}
 mkdir ${LOG_PATH}/update/
 
-echo -e "${COLOR_Yellow}${TIME}" >> ${LOG_PATH}/update//${DATE}.log
+echo -e "${COLOR_Yellow}${TIME}${COLOR_RESET}" >> ${LOG_PATH}/update//${DATE}.log
 bash <(curl -L -s ${SCRIPT_URL}) >> ${LOG_PATH}/update//${DATE}.log
 find ${LOG_PATH}/update/ -mtime +${MAXLOGDAY} -name "*.log" -exec rm {} \; # Delete outdate logs
 
